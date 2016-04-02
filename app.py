c64cce3627fa0da6b83de5004e000c66bcd7b616
Patrik Ackland <patrikackland@gmail.com>
2016-04-02 00:20:35 -0700

    Added routes

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/issues', methods=['POST'])
+def issues():
+    raise NotImplementedError
+
+@app.route('/results')
+def results():
+    raise NotImplementedError
+
 if __name__ == '__main__':
     app.run(debug=True)
