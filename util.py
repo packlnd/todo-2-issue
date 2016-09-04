0808a2f9d58faf3644ce7bf9a09427d470f9b49e
Patrik Ackland <patrikackland@gmail.com>
2016-09-04 13:55:54 -0700

    Initial work on util file

--- util.py
+++ util.py
@@ -1,8 +1,11 @@
 def user_logged_in():
     return False
 
-def get_repos():
-    return {}
+def get_repos(user, repo):
+    github.get('repos/%s/' % user)
+
+def get_issues(user, repo):
+    github.get('repos/%s/%s/issues' % (user, repo))
 
 def read_config():
     return {}
