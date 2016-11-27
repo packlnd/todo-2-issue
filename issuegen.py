802cfbd11a88b9d4502d1b3f745168effe8321ad
Patrik Ackland <patrikackland@gmail.com>
2016-11-27 19:25:08 +0100

    Get TODOs

--- issuegen.py
+++ issuegen.py
@@ -1,13 +1,5 @@
-import requests
-#POST https://api.github.com/repos/:owner/:repo/issues
-
-def github_post(ep, params):
+def todos_from_file(f):
     raise NotImplementedError
 
-def main():
-    endpoint = 'repos/%s/%s/issues' % ('packlnd','todo-2-issue')
-    github_post(endpoint, {
-        'title':'[VINCENT] Issue',
-        'body':'This is the body of my issue',
-        'assignee':'packlnd'
-    })
+def parse_line():
+    raise NotImplementedError
